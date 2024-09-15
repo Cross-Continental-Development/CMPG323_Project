f539730d84114b062fc10b665dfa9834390d08ee
Fonteinman <21088799@student.g.nwu.ac.za>
2024-09-15 10:54:31 +0200

    Initial Commit

---
+++ roles.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = process.env.PORT || 3306;
+
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: '102.222.124.17',
+    user: 'xcondea8o1p9_andre',
+    password: 'K9#gxoNOW@Mw',
+    database: 'xcondea8o1p9_SHARE2TEACH',
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on https://102.222.124.17:${PORT}`);
+});
+
+
+// Create a new ROLE
+app.post('/ROLES', (req, res) => {
+    const { name, description } = req.body;
+    db.query('INSERT INTO ROLES (ROLES.ROLE_NAME, ROLES.DESCRIPTION) VALUES (?, ?)', [name, description], (err, result) => {
+        if (err) throw err;
+        res.json({ message: 'ROLE created successfully', id: result.insertId });
+    });
+});
+
+
+// Get all ROLES
+app.get('/ROLES', (req, res) => {
+    db.query('SELECT * FROM ROLES', (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// Get specific ROLES
+app.get('/ROLES', (req, res) => {
+    const {search_id } = req.body;
+    db.query('SELECT * FROM ROLES WHERE ROLES.ID = ?',[search_id], (err, results) => {
+        if (err) throw err;
+        res.json(results);
+    });
+});
+
+// Update ROLE description
+app.put('/ROLES', (req, res) => {
+    const {search_id, name, description } = req.body;
+    db.query('UPDATE ROLES SET ROLES.ROLE_NAME = ?, ROLES.DESCRIPTION = ? WHERE ROLES.ID = ?',[name, description, search_id], (err, results) => {
+        if (err) throw err;
+        res.json({ message: 'ROLE updated successfully', id: results.insertId });
+    });
+});
+
+// Delete ROLE description
+app.delete('/ROLES', (req, res) => {
+    const {search_id} = req.body;
+    db.query('DELETE FROM ROLES WHERE ROLES.ID = ?',[search_id], (err, results) => {
+        if (err) throw err;
+        res.json({ message: 'ROLE updated successfully', id: results.insertId });
+    });
+});
+
